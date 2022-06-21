aa815a03ccbf960a633527599b5718ce43112ddd
Jaegoomon <jaegoo1199@gmail.com>
2022-06-21 21:41:48 +0900

    feat: member service HttpServlet

--- src/test/java/hello/servlet/servlet/domain/MemberRepositoryTest.java
+++ src/test/java/hello/servlet/servlet/domain/MemberRepositoryTest.java
@@ -1,4 +1,47 @@
-import static org.junit.jupiter.api.Assertions.*;
+package hello.servlet.servlet.domain;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
 class MemberRepositoryTest {
-  
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @AfterEach
+    void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void save() {
+        // given
+        Member member = new Member("조재구", 25);
+
+        // when
+        Member savedMember = memberRepository.save(member);
+
+        // then
+        Member findMember = memberRepository.findById(savedMember.getId());
+        assertThat(savedMember).isEqualTo(findMember);
+    }
+
+    @Test
+    void findAll() {
+        // given
+        Member member1 = new Member("조재구", 25);
+        Member member2 = new Member("조재혁", 28);
+
+        // when
+        Member savedMember1 = memberRepository.save(member1);
+        Member savedMember2 = memberRepository.save(member2);
+
+        // then
+        List<Member> members = memberRepository.findAll();
+        assertThat(members).contains(savedMember1);
+        assertThat(members).contains(savedMember2);
+    }
 }
